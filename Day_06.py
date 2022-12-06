56b3c6f57b0eb26c26142e5690709b3edd140250
Roman Buchta <103024512+Linuxer72@users.noreply.github.com>
2022-12-06 17:09:13 +0100

    Update Day_06.py

--- Day_06.py
+++ Day_06.py
@@ -5,25 +5,16 @@ start = pfc()
 with open ("input.txt") as f:
     data = f.read()
     
-########  Part 1  ###################
-def search_of_four(data):
-    for i in range(0, len(data)-4):
+#########  Part 1 & 2  ##################        
+def search_of_(data, length):
+    for i in range(0, len(data)-length):
         chars = set()
-        for j in range(4):
+        for j in range(length):
             chars.add(data[i+j])
-        if len(chars) == 4:
-            return (i+j)+1
-#####################################
-#########  Part 2  ##################        
-def search_of_fourteen(data):
-    for i in range(0, len(data)-14):
-        chars = set()
-        for j in range(14):
-            chars.add(data[i+j])
-        if len(chars) == 14:
+        if len(chars) == length:
             return (i+j)+1        
         
         
-print("Part_1: ", search_of_four(data))
-print("Part_2: ", search_of_fourteen(data))
+print("Part_1: ", search_of_(data, 4))
+print("Part_2: ", search_of_(data, 14))
 print("Time: ", (pfc()-start), "seconds")
